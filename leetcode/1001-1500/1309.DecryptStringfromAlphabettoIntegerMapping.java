1010a5e8cd488fa51b52293ab256eff8d7eb0ef5
Deepansh Singh <deepansh54@gmail.com>
2020-11-08 00:22:24 +0530

    Day 46 leetcode #100DaysOfCode

--- leetcode/1001-1500/1309.DecryptStringfromAlphabettoIntegerMapping.java
+++ leetcode/1001-1500/1309.DecryptStringfromAlphabettoIntegerMapping.java
@@ -1,12 +1,17 @@
 class Solution {
-    public String freqAlphabets(String s) {        
-        for(int i=10; i<27;i++){
-            s = s.replaceAll(String.valueOf(i)+"#", String.valueOf((char)(i+96)));
+    public String freqAlphabets(String s) {
+        StringBuilder r = new StringBuilder();
+        for(int i=0;i<s.length();){
+            if(i+2 < s.length() && s.charAt(i+2)=='#'){
+                r.append((char)((s.charAt(i)-48)*10+(s.charAt(i+1)-48)+96));
+                i+=3;
+            }
+            else{
+                r.append((char)(s.charAt(i)+48));
+                i++;
+            }
         }
-        for(int i=1; i<10; i++){
-            s = s.replaceAll(String.valueOf(i), String.valueOf((char)(i+96)));
-        }
-        return s;
+        return r.toString();
     }
 }
 //https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
