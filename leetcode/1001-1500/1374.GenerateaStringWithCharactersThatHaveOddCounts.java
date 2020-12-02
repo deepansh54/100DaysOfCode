008410f8050152f2bbf63b91deb1993fcb115558
Deepansh Singh <deepansh54@gmail.com>
2020-12-03 00:03:23 +0530

    Day 71 leetcode #100DaysOfCode

--- leetcode/1001-1500/1374.GenerateaStringWithCharactersThatHaveOddCounts.java
+++ leetcode/1001-1500/1374.GenerateaStringWithCharactersThatHaveOddCounts.java
@@ -1,26 +1,13 @@
 class Solution {
     public String generateTheString(int n) {
         StringBuilder s = new StringBuilder();
-        int len1 = 0;
-        int len2=0;
-        if(n==1){
-            return "a";
-        }
-        if(n==2){
-            return "ab";
-        }
         if(n%2==0){
-            len1 = n-1;
-            len2 = 1;
-            for(int i=0;i<len1;i++){
+            for(int i=0;i<n-1;i++){
                 s.append('a');
             }
-            for(int i=0;i<len2;i++){
-                s.append('b');
-            }
+            s.append('b');
         }
         else{
-            
             for(int i=0;i<n;i++){
                 s.append('c');
             }
