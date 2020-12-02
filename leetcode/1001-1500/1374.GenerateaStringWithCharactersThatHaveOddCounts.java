f4660d1a1dfe3beadf363f316979ee5f1373ed9c
Deepansh Singh <deepansh54@gmail.com>
2020-12-03 00:00:45 +0530

    Day 71 leetcode #100DaysOfCode

--- leetcode/1001-1500/1374.GenerateaStringWithCharactersThatHaveOddCounts.java
+++ leetcode/1001-1500/1374.GenerateaStringWithCharactersThatHaveOddCounts.java
@@ -3,19 +3,27 @@ class Solution {
         StringBuilder s = new StringBuilder();
         int len1 = 0;
         int len2=0;
+        if(n==1){
+            return "a";
+        }
+        if(n==2){
+            return "ab";
+        }
         if(n%2==0){
-            len1 = n/2;
-            len2 = n/2;
+            len1 = n-1;
+            len2 = 1;
+            for(int i=0;i<len1;i++){
+                s.append('a');
+            }
+            for(int i=0;i<len2;i++){
+                s.append('b');
+            }
         }
         else{
-            len1 = n/2;
-            len2 = n-n/2;
-        }
-        for(int i=0;i<len1;i++){
-            s.append('a');
-        }
-        for(int i=0;i<len2;i++){
-            s.append('b');
+            
+            for(int i=0;i<n;i++){
+                s.append('c');
+            }
         }
         return s.toString();
     }
