03f62de5f6b841df41ba6b68d0856b89c6ec1b05
Deepansh Singh <deepansh54@gmail.com>
2020-12-15 23:22:56 +0530

    Day 84 leetcode #100DaysOfCode

--- leetcode/501-1000/977.SquaresofaSortedArray.java
+++ leetcode/501-1000/977.SquaresofaSortedArray.java
@@ -1,16 +1,41 @@
 class Solution {
-    public int[] sortedSquares(int[] A) {
-        int m = -1;
-        for(int i=0;i<A.length;i++){
-            if(A[i]<0){
-                m = i;
+    public int[] sortedSquares(int[] nums) {
+        int lastNegativeIndex = -1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                lastNegativeIndex = i;
             }
-            A[i] = A[i]*A[i];
+            nums[i]*=nums[i];
         }
-        if(m>-1){
-            Arrays.sort(A);
-        }        
-        return A;
+        if(lastNegativeIndex == -1){
+            return nums;
+        }
+        int[] sorted= new int[nums.length];
+        int i = lastNegativeIndex; 
+        int j = lastNegativeIndex+1;
+        int k = 0;
+        while(i>=0 && j<nums.length){
+            if(nums[i]<=nums[j]){
+                sorted[k] = nums[i];
+                i--;
+            }
+            else{
+                sorted[k] = nums[j];
+                j++;
+            }
+            k++;
+        }
+        while(i>=0){
+            sorted[k] = nums[i];
+            i--;
+            k++;
+        }
+        while(j<nums.length){
+            sorted[k] = nums[j];
+            j++;
+            k++;
+        }
+        return sorted;
     }
 }
 //https://leetcode.com/problems/squares-of-a-sorted-array/
