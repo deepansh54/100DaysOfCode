8515a4e45ab8717d0b8008612f15b36ec030074e
Deepansh Singh <deepansh54@gmail.com>
2021-01-01 22:22:28 +0530

    Day 101 leetcode #100DaysOfCode

---
+++ leetcode/1501-2000/1640.CheckArrayFormationThroughConcatenation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        HashMap<Integer, Integer> piecesMap = new HashMap<>();
+        for(int i=0;i<pieces.length;i++){
+            piecesMap.put(pieces[i][0], i);
+        }
+        //System.out.println(piecesMap);
+        for(int i=0;i<arr.length;){
+            Integer p = piecesMap.get(arr[i]);
+            if(p==null){
+                return false;
+            }
+            else{
+                for(int j=0;j<pieces[p].length;j++){
+                    if(arr[i] == pieces[p][j]){
+                        i++;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+//https://leetcode.com/submissions/detail/437163819/
