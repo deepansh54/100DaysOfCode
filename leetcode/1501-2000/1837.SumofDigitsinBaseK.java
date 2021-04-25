ca350896cc95e5ca385b080043794acde8da174b
Deepansh Singh <deepansh54@gmail.com>
2021-04-25 23:33:04 +0530

    Day 215 leetcode #100DaysOfCode

---
+++ leetcode/1501-2000/1837.SumofDigitsinBaseK.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sumBase(int n, int k) {
+        int sum = 0;
+        while(n>0){
+            sum+=(n%k);
+            n/=k;
+        }
+        return sum;
+    }
+}
+//https://leetcode.com/problems/sum-of-digits-in-base-k/
